4b1b445351b571a6a7651faf654b48cf73db5805
thaismooura <thaismooura01@gmail.com>
2026-06-18 19:41:36 -0300

    primeiro commit

---
+++ src/app/components/Post.tsx
@@ -0,0 +1,17 @@
+import styles from "./Post.module.css";
+
+interface PostProps {
+  author: string;
+  body?: string;
+}
+
+function Post(props: PostProps) {
+  return (
+    <div className={styles.post}>
+      <p className={styles.author}>{props.author}</p>
+      <p className={styles.body}>{props.body}</p>
+    </div>
+  );
+}
+
+export default Post;
